6eeac398df452c30e30e17a356f1309b4d1ab4f3
julienlopez <julien.lopez51@gmail.com>
2020-12-11 14:01:21 +0100

    setting things up

---
+++ libs/libwarhammerengine/characteristics.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cstdint>
+
+namespace QWarhammerSimulator::LibWarhammerEngine
+{
+
+struct Characteristics
+{
+	using Type = std::int8_t;
+	
+	// Movement (M)
+	Type m;
+
+	// Weapon Skill (WS)
+	Type ws;
+
+	// Ballistic Skill (BS)
+	Type bs;
+
+	// Strength (S)
+	Type s;
+
+	// Toughness (T)
+	Type t;
+
+	// Wounds (W)
+	Type w;
+
+	// INITIATIVE (I)
+	Type i;
+
+	// ATTACKS (A)
+	Type a;
+
+	// LEADERSHIP (Ld)
+	Type ld;
+};
+
+} // namespace CppGraphQL::LibClient
