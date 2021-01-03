8908bb154752b8e7ec969d375ff7bf835304f1e8
julienlopez <julien.lopez51@gmail.com>
2021-01-03 19:33:44 +0100

    Create army.hpp

---
+++ libs/libwarhammerengine/army.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "unit.hpp"
+
+#include <vector>
+
+namespace QWarhammerSimulator::LibWarhammerEngine
+{
+
+class Army
+{
+public:
+
+private:
+    std::string name;
+    std::vector<Unit> m_units;
+};
+
+} // namespace QWarhammerSimulator::LibWarhammerEngine
