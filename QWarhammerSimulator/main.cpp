5c52618f32a3f6599b333a2ba6f6f24a827afc90
julienlopez <julien.lopez51@gmail.com>
2021-07-18 11:42:56 +0200

    updating the starting armies for a better view

--- QWarhammerSimulator/main.cpp
+++ QWarhammerSimulator/main.cpp
@@ -6,6 +6,9 @@
 
 namespace
 {
+
+const double pi = std::acos(-1);
+
 auto setupEmpireArmy()
 {
     using namespace QWarhammerSimulator::LibWarhammerEngine;
@@ -16,8 +19,9 @@ auto setupEmpireArmy()
 
     Model empire_halberdier{"empire halberdier", Point{1, 1}, Characteristics{4, 3, 3, 3, 3, 1, 3, 1, 7}};
 
-    army.m_units.push_back({empire_halberdier, 20, 5, {10., 10.}});
-    army.m_units.push_back({empire_halberdier, 20, 5, {30., 10.}});
+    army.m_units.push_back({empire_halberdier, 20,  5, {20., 10.}, -pi/2});
+    army.m_units.push_back({empire_halberdier, 20, 5, {40., 10.}, -pi / 2});
+    army.m_units.push_back({empire_halberdier, 20, 5, {60., 10.}, -pi / 2});
 
     return army;
 }
@@ -28,7 +32,13 @@ auto setupOrcArmy()
     using namespace QWarhammerSimulator::LibGeometry;
 
     Army army;
-    army.name = "The Orc horde";
+    army.name = "The Skaven horde";
+
+    Model orc_boyz{"Skaven clanrat", Point{1, 1}, Characteristics{5, 3, 3, 3, 3, 1, 4, 1, 5}};
+
+    army.m_units.push_back({orc_boyz, 20, 5, {20., 40.}, pi / 2});
+    army.m_units.push_back({orc_boyz, 20, 5, {40., 40.}, pi / 2});
+    army.m_units.push_back({orc_boyz, 20, 5, {60., 40.}, pi / 2});
 
     return army;
 }
@@ -38,7 +48,7 @@ auto setupGame()
     using namespace QWarhammerSimulator::LibWarhammerEngine;
     using namespace QWarhammerSimulator::LibGeometry;
 
-    auto game = std::make_unique<Game>(GameBoard{75, 50});
+    auto game = std::make_unique<Game>(GameBoard{80, 50});
     game->army(0) = setupEmpireArmy();
     game->army(1) = setupOrcArmy();
 
